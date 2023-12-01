9eea3d294a6f149a805ceede7866da757df7c4f2
EinMilchBoss <christian.benezuela@gmail.com>
2023-12-01 16:03:45 +0100

    Refactor part 2.

--- Util/Aoc/Challenge.cs
+++ Util/Aoc/Challenge.cs
@@ -5,6 +5,7 @@ public record Challenge(int year, int day)
     public string ReadInput(string filename)
     {
         var paddedDay = day.ToString().PadLeft(2, '0');
-        return File.ReadAllText($"./res/Year{year}/Day{paddedDay}/{filename}");
+        // Probably not the best idea, but this lets me debug in peace at least.
+        return File.ReadAllText($"{AppDomain.CurrentDomain.BaseDirectory}/../../../../../res/Year{year}/Day{paddedDay}/{filename}");
     }
 }
--- Year2023/Day01/Program.cs
+++ Year2023/Day01/Program.cs
@@ -1,132 +1,139 @@
-﻿using System.Diagnostics;
-using Util.Aoc;
+﻿using Util.Aoc;
 
-public static class Program
+Dictionary<string, int> NUMBER_WORDS = new()
 {
-    public static void Main()
-    {
-        var challenge = new Challenge(2023, 1);
-        var example1 = challenge.ReadInput("example1.txt");
-        var example2 = challenge.ReadInput("example2.txt");
-        var actual = challenge.ReadInput("actual.txt");
+    ["one"] = 1,
+    ["two"] = 2,
+    ["three"] = 3,
+    ["four"] = 4,
+    ["five"] = 5,
+    ["six"] = 6,
+    ["seven"] = 7,
+    ["eight"] = 8,
+    ["nine"] = 9,
+};
 
-        var one = new Part<int>("Calibration sum", PartOne);
-        var two = new Part<int>("Advanced calibration sum", PartTwo);
+var challenge = new Challenge(2023, 1);
+var example1 = challenge.ReadInput("example1.txt");
+var example2 = challenge.ReadInput("example2.txt");
+var actual = challenge.ReadInput("actual.txt");
 
-        Console.WriteLine(one.Test(142, example1));
-        Console.WriteLine(two.Test(281, example2));
+var one = new Part<int>("Calibration sum", PartOne);
+var two = new Part<int>("Advanced calibration sum", PartTwo);
 
-        Console.WriteLine(one.Run(actual));
-        Console.WriteLine(two.Run(actual));
-    }
+Console.WriteLine(one.Test(142, example1));
+Console.WriteLine(two.Test(281, example2));
 
-    private static int PartOne(string input)
-    {
-        return input.Split(Environment.NewLine)
-            .Select((line) => FindFirstNumber(line) * 10 + FindLastNumber(line))
-            .Sum();
-    }
+Console.WriteLine(one.Run(actual));
+Console.WriteLine(two.Run(actual));
 
-    private static int FindFirstNumber(string line)
-    {
-        for (var i = 0; i < line.Length; i += 1)
+int PartOne(string input)
+{
+    return input.Split(Environment.NewLine)
+        .Select((line) =>
         {
-            if (char.IsDigit(line[i]))
-            {
-                return line[i] - '0';
-            }
-        }
-        throw new UnreachableException("Line has to contain at least one number.");
-    }
-
-    private static int FindLastNumber(string line)
-    {
-        for (var i = line.Length - 1; i >= 0; i -= 1)
-        {
-            if (char.IsDigit(line[i]))
-            {
-                return line[i] - '0';
-            }
-        }
-        throw new UnreachableException("Line has to contain at least one number.");
-    }
-
-    private static int PartTwo(string input)
-    {
-        var numberWords = new Dictionary<string, int>()
-        {
-            ["one"] = 1,
-            ["two"] = 2,
-            ["three"] = 3,
-            ["four"] = 4,
-            ["five"] = 5,
-            ["six"] = 6,
-            ["seven"] = 7,
-            ["eight"] = 8,
-            ["nine"] = 9,
-        };
-
-        var sum = 0;
-        var lines = input.Split(Environment.NewLine);
-        foreach (var line in lines)
-        {
-            int first = 0, last = 0;
-            int firstIndex = line.Length - 1, lastIndex = 0;
-
-            // durchsuche nach wörtern
-            for (var i = 0; i < line.Length; i += 1)
-            {
-                foreach (var word in numberWords.Keys)
-                {
-                    //Console.WriteLine($"{i} {word}");
-                    if (line.Substring(i).StartsWith(word))
-                    {
-                        first = numberWords[word];
-                        firstIndex = i;
-                        goto FoundFirstWord;
-                    }
-                }
-            }
-        FoundFirstWord:;
-
-            for (var i = line.Length - 1; i >= 0; i -= 1)
-            {
-                foreach (var word in numberWords.Keys)
-                {
-                    if (line.Substring(i).StartsWith(word))
-                    {
-                        last = numberWords[word];
-                        lastIndex = i;
-                        goto FoundLastWord;
-                    }
-                }
-            }
-        FoundLastWord:;
-
-            // durchsuchen nach zahlen
-            for (var i = 0; i <= firstIndex; i += 1)
-            {
-                if (char.IsDigit(line[i]))
-                {
-                    first = line[i] - '0';
-                    break;
-                }
-            }
-
-            for (var i = line.Length - 1; i >= lastIndex; i -= 1)
-            {
-                if (char.IsDigit(line[i]))
-                {
-                    last = line[i] - '0';
-                    break;
-                }
-            }
-            //Console.WriteLine(first * 10 + last);
-
-            sum += first * 10 + last;
-        }
-
-
-        return sum;
-    }
+            var (_, first) = FindFirstNumber(line)!.Value;
+            var (_, last) = FindLastNumber(line)!.Value;
+            return first * 10 + last;
+        })
+        .Sum();
 }
+
+int PartTwo(string input)
+{
+    /*
+    For part two they provided an example input that, for some reason, contains line(s) that have no number in them.
+    Therefore we have to return and check for null values every time.
+    We could theoretically switch the check for number words with numbers, but that would imply runtime cost.
+    Not that it would matter, but you know...
+    */
+
+    return input.Split(Environment.NewLine)
+        .Select((line) =>
+        {
+            var first = 0;
+            var last = 0;
+
+            var firstIndex = line.Length - 1;
+            var lastIndex = 0;
+
+            if (FindFirstNumber(line) is Result firstResult)
+            {
+                first = firstResult.Number;
+                firstIndex = firstResult.Index;
+            }
+            if (FindFirstNumberWord(line, firstIndex) is int newFirst)
+            {
+                first = newFirst;
+            }
+
+            if (FindLastNumber(line) is Result lastResult)
+            {
+                last = lastResult.Number;
+                lastIndex = lastResult.Index;
+            }
+            if (FindLastNumberWord(line, lastIndex) is int newLast)
+            {
+                last = newLast;
+            }
+
+            return first * 10 + last;
+        })
+        .Sum();
+}
+
+Result? FindFirstNumber(string line)
+{
+    for (var i = 0; i < line.Length; i += 1)
+    {
+        if (char.IsDigit(line[i]))
+        {
+            return new Result(i, line[i] - '0');
+        }
+    }
+    return null;
+}
+
+Result? FindLastNumber(string line)
+{
+    for (var i = line.Length - 1; i >= 0; i -= 1)
+    {
+        if (char.IsDigit(line[i]))
+        {
+            return new Result(i, line[i] - '0');
+        }
+    }
+    return null;
+}
+
+int? FindFirstNumberWord(string line, int until)
+{
+    for (var i = 0; i < until; i += 1)
+    {
+        foreach (var word in NUMBER_WORDS.Keys)
+        {
+            if (line[i..].StartsWith(word))
+            {
+                return NUMBER_WORDS[word];
+            }
+        }
+    }
+    return null;
+}
+
+int? FindLastNumberWord(string line, int until)
+{
+    for (var i = line.Length - 1; i > until; i -= 1)
+    {
+        foreach (var word in NUMBER_WORDS.Keys)
+        {
+            if (line[i..].StartsWith(word))
+            {
+                return NUMBER_WORDS[word];
+            }
+        }
+    }
+    return null;
+}
+
+readonly record struct Result(int Index, int Number);
