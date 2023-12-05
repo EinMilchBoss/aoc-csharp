6adae6a76bdc1dc468fe9283c3256fc9cbfc0bef
EinMilchBoss <christian.benezuela@gmail.com>
2023-12-05 10:44:49 +0100

    Add 23/04 part 1.

---
+++ Year2023/Day04/Card.cs
@@ -0,0 +1,46 @@
+using System.Text;
+
+namespace Day04;
+
+public readonly record struct Card(IEnumerable<int> Winning, IEnumerable<int> Actual)
+{
+    public static Card Parse(string line)
+    {
+        var sideOfNumbers = line.Split(": ")[1];
+        var numbers = sideOfNumbers.Split(" | ");
+        return new Card(ParseNumbers(numbers[0]), ParseNumbers(numbers[1]));
+    }
+
+    private static IEnumerable<int> ParseNumbers(string numbers)
+    {
+        const int NUMBER_LENGTH = 2;
+
+        IEnumerable<int> Iterate(int index)
+        {
+            if (index + NUMBER_LENGTH > numbers.Length)
+                return Enumerable.Empty<int>();
+
+            var number = numbers
+                .Substring(index, NUMBER_LENGTH)
+                .Replace(' ', '0');
+            return Iterate(index + 3).Prepend(int.Parse(number));
+        }
+
+        return Iterate(0);
+    }
+
+    public int Points()
+    {
+        var count = Winning
+            .Intersect(Actual)
+            .Count();
+        return (count == 0) ? 0 : (int)Math.Pow(2, count - 1);
+    }
+
+    public override string ToString() => GetLine(Winning)
+        .Append("| ")
+        .Append(GetLine(Actual).ToString())
+        .ToString();
+
+    private StringBuilder GetLine(IEnumerable<int> ints) => ints.Aggregate(new StringBuilder(), (a, b) => a.Append($"{b,2} "));
+}
---
+++ Year2023/Day04/Program.cs
@@ -0,0 +1,25 @@
+﻿using Day04;
+using Util.Aoc;
+
+var challenge = new Challenge(2023, 4);
+var example = challenge.ReadInput("example.txt");
+var actual = challenge.ReadInput("actual.txt");
+
+var one = new Part<int>("Sum of card points", PartOne);
+var two = new Part<int>("N/A", PartTwo);
+
+Console.WriteLine(one.Test(13, example));
+//Console.WriteLine(two.Test(-1, example));
+
+Console.WriteLine(one.Run(actual));
+//Console.WriteLine(two.Run(actual));
+
+int PartOne(string input) => input
+    .Split(Environment.NewLine)
+    .Select((line) => Card.Parse(line).Points())
+    .Sum();
+
+int PartTwo(string input)
+{
+    return 0;
+}
