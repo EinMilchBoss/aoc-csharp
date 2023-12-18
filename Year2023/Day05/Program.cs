855db12196ddd6b50f33b33a903ad08d0f50ea11
EinMilchBoss <christian.benezuela@gmail.com>
2023-12-18 17:45:46 +0100

    Start with 2023/05.

---
+++ Year2023/Day05/Program.cs
@@ -0,0 +1,26 @@
+﻿using Util.Aoc;
+
+var challenge = new Challenge(2023, 5);
+var example = challenge.ReadInput("example.txt");
+var actual = challenge.ReadInput("actual.txt");
+
+var one = new Part<int>("Nearest location", PartOne);
+var two = new Part<int>("N/A", PartTwo);
+
+Console.WriteLine(one.Test(-1, example));
+//Console.WriteLine(two.Test(-1, example));
+
+Console.WriteLine(one.Run(actual));
+//Console.WriteLine(two.Run(actual));
+
+int PartOne(string input)
+{
+    Console.WriteLine(input);
+
+    return 0;
+}
+
+int PartTwo(string input)
+{
+    return 0;
+}
